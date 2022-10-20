6f523c9cdf6b0664f60983ed3afbb0249d5f9f88
Jérémy Novice <jeremy.novice@easy-info.com>
2022-10-20 17:05:31 +0200

    Migration push - Jen - 20102022

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $carbon_in = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCarbonIn(): ?\DateTimeInterface
+    {
+        return $this->carbon_in;
+    }
+
+    public function setCarbonIn(\DateTimeInterface $carbon_in): self
+    {
+        $this->carbon_in = $carbon_in;
+
+        return $this;
+    }
+}
