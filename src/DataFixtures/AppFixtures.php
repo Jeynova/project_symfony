8a63fd79d2c3ac09e09e3ac415dea97bcb493660
Jérémy Novice <jeremy.novice@easy-info.com>
2022-10-26 17:38:15 +0200

    Receipe - JEN - 26102022

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Receipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -23,12 +24,30 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager) : void
     {
-
+        $ingredients = array();
         for ($i=0; $i <=25 ; $i++) { 
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
                        ->setPrice($this->faker->randomFloat(1, 1, 100));
+                       $ingredients[]=$ingredient;
             $manager->persist($ingredient);
+        }
+        $receipe = new Receipe();
+        for ($j=0; $j <=12 ; $j++) { 
+            $receipe = new Receipe();
+            $receipe->setName($this->faker->word())
+                       ->setPrice($this->faker->randomFloat(1, 1, 100))
+                       ->setTime(mt_rand(0,1) == 1 ? mt_rand(1,1440) : null)
+                       ->setPeople(mt_rand(0,1) == 1 ? mt_rand(1,50) : null)
+                       ->setDifficulty(mt_rand(0,1) == 1 ? mt_rand(1,5) : null)
+                       ->setDescription($this->faker->text(300))
+                       ->setPrice(mt_rand(0,1) == 1 ? mt_rand(1,1000) : null)
+                       ->setIsfavorite(mt_rand(0,1) == 1 ? 1 : 0);
+                       
+            for ($k=0; $k < mt_rand(5,15); $k++) { 
+                $receipe->addIngredient($ingredients[mt_rand(0,count($ingredients)-1)]);
+            }
+            $manager->persist($receipe);
         }
 
         $manager->flush();
---
+++ src/Entity/Receipe.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReceipeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[UniqueEntity('name')]
+#[ORM\HasLifecycleCallbacks]
+#[ORM\Entity(repositoryClass: ReceipeRepository::class)]
+class Receipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min:2,max:50)]
+    #[Assert\NotBlank()]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThanOrEqual(1441)]
+    private ?int $time = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThanOrEqual(51)]
+    private ?int $people = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThanOrEqual(6)]
+    private ?int $difficulty = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank()]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThanOrEqual(1001)]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?bool $isfavorite = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Assert\NotNull()]
+    private ?\DateTimeImmutable $createdate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Assert\NotNull()]
+    private ?\DateTimeImmutable $modifyat = null;
+
+    #[ORM\ManyToMany(targetEntity: Ingredient::class)]
+    private Collection $ingredients;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+        $this->createdate = new \DateTimeImmutable();
+        $this->modifyat = new \DateTimeImmutable();
+    }
+
+    #[ORM\PrePersist]
+    public function setModifyValue(){
+        $this->modifyat = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(?int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getPeople(): ?int
+    {
+        return $this->people;
+    }
+
+    public function setPeople(?int $people): self
+    {
+        $this->people = $people;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(?int $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isIsfavorite(): ?bool
+    {
+        return $this->isfavorite;
+    }
+
+    public function setIsfavorite(bool $isfavorite): self
+    {
+        $this->isfavorite = $isfavorite;
+
+        return $this;
+    }
+
+    public function getCreatedate(): ?\DateTimeInterface
+    {
+        return $this->createdate;
+    }
+
+    public function setCreatedate(\DateTimeInterface $createdate): self
+    {
+        $this->createdate = $createdate;
+
+        return $this;
+    }
+
+    public function getModifyat(): ?\DateTimeInterface
+    {
+        return $this->modifyat;
+    }
+
+    public function setModifyat(\DateTimeInterface $modifyat): self
+    {
+        $this->modifyat = $modifyat;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ingredient>
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(Ingredient $ingredient): self
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients->add($ingredient);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(Ingredient $ingredient): self
+    {
+        $this->ingredients->removeElement($ingredient);
+
+        return $this;
+    }
+}
